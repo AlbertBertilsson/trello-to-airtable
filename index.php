5a54f9a4ae5f43e1c09ded0c79497ab7c40053f2
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-22 15:36:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<html>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
 <?php
 echo getenv('test-key');
+echo "<br>";
+while (list($var,$value) = each ($_SERVER)) {
+  echo "$var => $value <br />";
+}
 ?>
+  </body>
+</html>
