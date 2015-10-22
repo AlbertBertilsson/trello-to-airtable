e93176a3446a3650f23396fca87047b3d0377a3c
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-22 15:45:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,18 @@
   </head>
   <body>
 <?php
+if (strtolower($_SERVER["HTTP_X_FORWARDED_PROTO"])) != 'https') {
+  echo "HTTPS only!";
+  http_response_code(403);
+  exit(0);
+}
+/*
 echo getenv('test-key');
 echo "<br>";
 while (list($var,$value) = each ($_SERVER)) {
-  echo "$var => $value <br />";
+  echo "$var => $value <br>";
 }
+*/
 ?>
   </body>
 </html>
