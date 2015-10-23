04516a9aa86898f393afe3e3de00a25b51371204
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-23 11:22:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,27 +5,15 @@
   <body>
 <?php
 
-if (strtolower($_SERVER["HTTP_X_FORWARDED_PROTO"]) != "https") {
-  echo "HTTPS only!";
-  http_response_code(403);
-  exit(0);
-}
+$verbose = true;
 
-echo "HTTPS good!<br>";
-
-
-if ($_GET["debug"] != getenv("debug")) {
-  echo "Debug parameter required!";
-  http_response_code(403);
-  exit(0);
-}
-
-echo "Debug paramater good!<br><br>";
+if (strtolower($_SERVER["HTTP_X_FORWARDED_PROTO"]) != "https") $verbose = false;
+if ($_GET["debug"] != getenv("debug")) $verbose = false;
 
 
 //Get airtable
 $airtablelisturl = "https://api.airtable.com/v0/appq4IfZYs9aL2s1e/Incidents?view=Active";
-echo "Call: " . $airtablelisturl . "<br><br>";
+if ($verbose) echo "Call: " . $airtablelisturl . "<br><br>";
 
 $ch = curl_init($airtablelisturl);
 
@@ -46,29 +34,7 @@ if(curl_errno($ch))
 }
 curl_close ($ch);
 
-echo $atresult . "<br><br>";
-
-
-/*
-//We are not using the definition of the trello lists,
-//hard coded them instead because there is not a one to one mapping anyway.
-$trellolistsurl = "https://api.trello.com/1/boards/" . getenv("trello-board") . 
-  "?lists=open&list_fields=name&fields=name,desc&key=" . getenv("trello-key") . 
-  "&token=" . getenv("trello-token");
-
-echo "Call: " . $trellolistsurl . "<br><br>";
-
-$listsjson = file_get_contents($trellolistsurl);
-//echo $listsjson . "<br><br>";
-$lists = json_decode($listsjson);
-$listarr = array();
-for ($i = 0; $i < count($lists->{'lists'}); $i++) {
-  $listarr[$lists->{'lists'}[$i]->{'id'}] = $lists->{'lists'}[$i]->{'name'};
-  //echo "Lists: " . $lists->{'lists'}[$i]->{'id'} . " " . $lists->{'lists'}[$i]->{'name'} . "<br>";
-}
-//echo var_dump($listarr);
-//echo "<br><br>";
-*/
+if ($verbose) echo $atresult . "<br><br>";
 
 
 //Hard coded status for the trello lists, only active lists are included
@@ -115,15 +81,13 @@ $trellocardsurl = "https://api.trello.com/1/boards/" . getenv("trello-board") .
   "/cards?fields=name,idList&key=" . getenv("trello-key") . 
   "&token=" . getenv("trello-token");
 
-//echo "Call: " . $trellocardsurl . "<br><br>";
 $cardsjson = file_get_contents($trellocardsurl);
-//echo $cardsjson . "<br><br>";
 $cards = json_decode($cardsjson);
 
 //Go through all trello cards and calculate changes
 for ($i = 0; $i < count($cards); $i++) {
   if (!empty($listarr[$cards[$i]->{'idList'}])) {
-    echo $cards[$i]->{'id'} . ";" . 
+    if ($verbose) echo $cards[$i]->{'id'} . ";" . 
     get_cq($cards[$i]->{'name'}) . ";" .
     get_inc($cards[$i]->{'name'}) . ";" .
     get_title($cards[$i]->{'name'}) . ";" .
@@ -132,7 +96,38 @@ for ($i = 0; $i < count($cards); $i++) {
 }
 
 
+
+
+
+
+// Log to airtable
+$airtablelogurl = "https://api.airtable.com/v0/appq4IfZYs9aL2s1e/TrelloImportLog";
+if ($verbose) echo "Call: " . $airtablelogurl . "<br><br>";
+
+$ch = curl_init($airtablelogurl);
+
+$atheaders = array( 
+    "Authorization: Bearer " . getenv("airtable-key"),
+    "Content-type: application/json"
+);
+
+$payload = '{"fields": {"Entry": "Integration run!","Time": "' . date('Y-m-d H:i:s') . '"}}';
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $atheaders);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+
+$atresult = curl_exec($ch);
+
+if(curl_errno($ch))
+{
+  echo "Failed to get airtable! Curl error: " . curl_error($ch);
+  http_response_code(200);
+  exit(0);
+}
+curl_close ($ch);
+
+echo '<br><br>Done!';
+
 ?>
   </body>
 </html>
-
