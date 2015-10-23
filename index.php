2f17467884fd21fa0a0cf4f82e8e7e9e2b60e79d
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-23 12:54:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,29 +12,35 @@ if ($_GET["debug"] != getenv("debug")) $verbose = false;
 
 
 //Get airtable
-$airtablelisturl = "https://api.airtable.com/v0/appq4IfZYs9aL2s1e/Incidents?view=Active";
-//if ($verbose) echo "Call: " . $airtablelisturl . "<br><br>";
+function get_airtable() {
+  $airtablelisturl = "https://api.airtable.com/v0/appq4IfZYs9aL2s1e/Incidents?view=Active";
+  //if ($verbose) echo "Call: " . $airtablelisturl . "<br><br>";
 
-$ch = curl_init($airtablelisturl);
+  $ch = curl_init($airtablelisturl);
 
-$atheaders = array( 
-    "Authorization: Bearer " . getenv("airtable-key")
-);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $atheaders);
+  $atheaders = array( 
+      "Authorization: Bearer " . getenv("airtable-key")
+  );
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $atheaders);
 
 
-$atresult = curl_exec($ch);
+  $atresult = curl_exec($ch);
 
-if(curl_errno($ch))
-{
-  echo "Failed to get airtable! Curl error: " . curl_error($ch);
-  http_response_code(200);
-  exit(0);
+  if(curl_errno($ch))
+  {
+    echo "Failed to get airtable! Curl error: " . curl_error($ch);
+    http_response_code(200);
+    exit(0);
+  }
+  curl_close ($ch);
+
+  //if ($verbose) echo $atresult . "<br><br>";
+  return $atresult;
 }
-curl_close ($ch);
 
-//if ($verbose) echo $atresult . "<br><br>";
+//$atresult = get_airtable();
+
 
 
 //Hard coded status for the trello lists, only active lists are included
@@ -97,13 +103,14 @@ function log_airtable($line) {
   $atresult = curl_exec($ch);
 
   if(curl_errno($ch)) {
-    echo "Failed to get airtable! Curl error: " . curl_error($ch);
+    echo "Failed to log to airtable! Curl error: " . curl_error($ch);
   }
   curl_close ($ch);
 }
 
 
 // Update airtable
+/*
 function update_airtable($id, $payload) {
   $airtablelogurl = "https://api.airtable.com/v0/appq4IfZYs9aL2s1e/Incidents/" . $id;
   if ($verbose) echo "Call: " . $airtablelogurl . "<br><br>";
@@ -127,6 +134,8 @@ function update_airtable($id, $payload) {
   }
   curl_close ($ch);
 }
+*/
+
 
 //Get the cards
 $trellocardsurl = "https://api.trello.com/1/boards/" . getenv("trello-board") . 
@@ -136,7 +145,7 @@ $trellocardsurl = "https://api.trello.com/1/boards/" . getenv("trello-board") .
 //if ($verbose) echo $trellocardsurl . "<br><br>";
 $cardsjson = file_get_contents($trellocardsurl);
 //if ($verbose) echo $cardsjson . "<br><br>";
-//$cards = json_decode($cardsjson);
+$cards = json_decode($cardsjson);
 
 
 //Go through all trello cards and calculate changes
