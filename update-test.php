4fe716d166961fb2e000ac6f12ec9a7a1c0337db
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-26 11:03:10 +0100

    Update update-test.php

--- update-test.php
+++ update-test.php
@@ -15,6 +15,55 @@ if (isset($_SERVER["HTTP_HOST"]))
     $verbose = true;    
   }
 
+
+//Hard coded status for the trello lists, only active lists are included
+
+$listarr = array(
+  "55e5af5a7105ece0bb03d417" => "Open",
+  "55e58b8960a27158e41e7898" => "Open",
+  "55fc2a94fc78778668ac912e" => "Requires backend fix",
+  "55e58b8960a27158e41e789a" => "Fixed in Tealium",
+  "55e58b8960a27158e41e789b" => "Waiting for verification",
+  );
+
+//Functions for processing card name
+function get_cq($name) {
+  $matches = array();
+  preg_match('/\[CQ([\d]+)\]/', $name, $matches);
+  if (isset($matches[1]))
+    return "CQ" . $matches[1];
+
+  return "";
+}
+
+function get_inc($name) {
+  $matches = array();
+  preg_match('/\[INC([\d]+)\]/', $name, $matches);
+  if (isset($matches[1]))
+    return "INC" . $matches[1];
+
+  return "";
+}
+
+
+function get_title($name) {
+  $matches = array();
+  preg_match('/.*\](.*)/', $name, $matches);
+  if (isset($matches[1]))
+    if (strlen(trim($matches[1])) > 0)
+      return trim($matches[1]);
+
+  return $name;
+}
+
+function get_field($row, $field) {
+  if (isset($row->{$field}))
+    return $row->{$field};
+
+  return "";
+}
+
+
 // Update airtable
 function update_airtable($id, $payload) {
   global $verbose, $local;
@@ -61,6 +110,7 @@ function card_to_row_json($card) {
   return $json;
 }
 
-$card = json_decode('{"TrelloId":"561b6c1cb394acc46b1e5ea2","Title":"Quick View event not set for Quick View pages","Status":"Open","CQId":"CQ75"},"createdTime":"2015-10-23T12:25:21.000Z"}');
+$cards = json_decode('{"id":"561b6c1cb394acc46b1e5ea2","name":"(8) [CQ75] [INSERT ID] Quick View event not set for Quick View pages (2)","idList":"55e5af5a7105ece0bb03d417"}');
 $json = card_to_row_json($cards);
-update_airtable($id, $json);
+update_airtable("recLFGi40f8HHEycT", $json);
+
