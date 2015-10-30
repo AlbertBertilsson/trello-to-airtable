611c4949517bfcb4adb347da67edc52f16223940
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-30 16:05:37 +0100

    Update log.php

--- log.php
+++ log.php
@@ -1,19 +1,17 @@
 <?php
 
 function loggly_log($data) {
-  global $verbose;
-  
-  if ($verbose) echo $data . "<br><br>";
-  
   $logurl = "https://logs-01.loggly.com/inputs/" . getenv("loggly-token") . "/tag/" . $_SERVER["SERVER_NAME"] . "/";
 
   $ch = curl_init($logurl);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data );
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
   $atresult = curl_exec($ch);
+
   if(curl_errno($ch)) {
-    echo "Failed to log! Curl error: " . curl_error($ch);
+    echo "Failed to log! Curl error: " . curl_error($ch) . "<br><br>";
   }
+
   curl_close ($ch);
 }
