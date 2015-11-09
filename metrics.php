c00cf3f00a4a7dbdb6a45648ff5a5e16ac385fad
AlbertBertilsson <abbe_something@hotmail.com>
2015-11-09 17:22:53 +0100

    Update metrics.php

--- metrics.php
+++ metrics.php
@@ -57,20 +57,24 @@ function process_metric_variables($rec) {
         strtoupper(trim($vars[$v])) === "TBA")
       $use = false;
 
-    if (count($vars) === 0)
-      $use = false;
+  if (count($vars) === 0)
+    $use = false;
 
+  if ($verbose) {
     echo $rec->{'id'} . " - ";
     echo $rec->{'fields'}->{'Name'} . '<br>';
     echo $rec->{'fields'}->{'Variables'} . '<br>';
     if ($use) {
-      $variables[$rec->{'id'}] = $rec->{'fields'}->{'Variables'};
       for ($v = 0 ; $v < count($vars) ; $v++)
         echo "\"<strong>" . trim($vars[$v]) . "</strong>\", ";
     } else {
       echo "\"<strong>Do not use!</strong>\", ";
     }
     echo '<br><br>';
+  }
+
+  if ($use)
+      $variables[$rec->{'id'}] = $rec->{'fields'}->{'Variables'};
 }
 
 
@@ -90,49 +94,8 @@ do {
 } while (isset($json->{"offset"}));
 
 
-//Get url for trello
-function get_trello_url($url) {
-  if (strpos($url, '?') !== false)
-    $url = $url . "&key=";
-  else
-    $url = $url . "?key=";
 
-  $url = $url . getenv("trello-key") . "&token=" . getenv("trello-token");
-
-  return $url;  
-}
-
-function trello_put_index($payload) {
-  global $verbose, $local;
-
-  if ($local) return;
-
-  $url = "https://api.trello.com/1/cards/PPa9dRFM/desc";
-  $url = get_trello_url($url);
-  $url .= '&value=' . urlencode($payload);
-  if ($verbose) echo "Call: " . $url . "<br><br>";
-
-  $ch = curl_init($url);
-
-  $atheaders = array( 
-      "Content-type: application/json"
-  );
-
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
-  $atresult = curl_exec($ch);
-
-  if(curl_errno($ch)) {
-    echo "Failed to create row in airtable! Curl error: " . curl_error($ch);
-  }
-  curl_close ($ch);
-}
-
-
-trello_put_index(json_encode($variables));
-//echo json_encode($variables);
+if ($verbose) echo json_encode($variables);
 
 echo 'Done!';
 
