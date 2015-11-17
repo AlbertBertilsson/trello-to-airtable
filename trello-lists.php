b701da26584b2fc0f9d3f27d66ee3088b4c2cd9e
AlbertBertilsson <abbe_something@hotmail.com>
2015-11-17 10:50:40 +0100

    Create trello-lists.php

---
+++ trello-lists.php
@@ -0,0 +1,23 @@
+<?php
+
+$trello_list_legend = "55e58b8960a27158e41e7897";
+$trello_list_ongoing = "55e5af5a7105ece0bb03d417";
+$trello_list_reported_idesk = "55e58b8960a27158e41e7898";
+$trello_list_requires_backend = "55fc2a94fc78778668ac912e";
+$trello_list_fixed_tealium = "55e58b8960a27158e41e789a";
+$trello_list_resolved_slf = "55e58b8960a27158e41e789b";
+$trello_list_validated = "55e5ab9aa12abb4548ba60f9";
+$trello_list_resolved = "55e6b509ff8d0e999ce55d64";
+
+$trello_active_lists = array(
+	$trello_list_ongoing,
+	$trello_list_reported_idesk,
+	$trello_list_requires_backend,
+	$trello_list_fixed_tealium
+);
+
+$trello_affecting_lists = array(
+	$trello_list_ongoing,
+	$trello_list_reported_idesk,
+	$trello_list_requires_backend
+);
