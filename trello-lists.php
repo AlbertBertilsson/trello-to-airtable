dc4af552eda7284649e375bdbb10e56bce65d3b7
AlbertBertilsson <abbe_something@hotmail.com>
2015-12-04 11:28:22 +0100

    Update trello-lists.php
    Added the list "Rejeced iDesk".

--- trello-lists.php
+++ trello-lists.php
@@ -3,6 +3,7 @@
 $trello_list_legend = "55e58b8960a27158e41e7897";
 $trello_list_ongoing = "55e5af5a7105ece0bb03d417";
 $trello_list_reported_idesk = "55e58b8960a27158e41e7898";
+$trello_list_rejected_idesk = "56581c4273241889bf726215";
 $trello_list_requires_backend = "55fc2a94fc78778668ac912e";
 $trello_list_fixed_tealium = "55e58b8960a27158e41e789a";
 $trello_list_resolved_slf = "55e58b8960a27158e41e789b";
@@ -12,6 +13,7 @@ $trello_list_resolved = "55e6b509ff8d0e999ce55d64";
 $trello_active_lists = array(
 	$trello_list_ongoing,
 	$trello_list_reported_idesk,
+	$trello_list_rejected_idesk,
 	$trello_list_requires_backend,
 	$trello_list_fixed_tealium
 );
@@ -19,5 +21,6 @@ $trello_active_lists = array(
 $trello_affecting_lists = array(
 	$trello_list_ongoing,
 	$trello_list_reported_idesk,
+	$trello_list_rejected_idesk,
 	$trello_list_requires_backend
 );
